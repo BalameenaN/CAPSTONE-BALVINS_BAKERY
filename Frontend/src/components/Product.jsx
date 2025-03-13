802e18a2263c9aa235ce1d16d526fd1f0e427fbc
BalameenaN <balameena.navukkarasu@gmail.com>
2025-03-13 15:06:32 -0600

    product filtering logic

--- Frontend/src/components/Product.jsx
+++ Frontend/src/components/Product.jsx
@@ -21,27 +21,48 @@ export default function Product(){
         getProduct();
     },[]);
 
-    console.log(product);
+    //console.log(product);
+    
+    //function to create JSX element for each product
+    function productMatching(prod){
+        const result = prod.map((p)=>{
+            return(
+                <>
+                <div className='product-container'>
+                <img src={p.Image_URL}/>
+               <h3>{p.Name}</h3>
+                <h5>Price: ${p.Price}</h5>
+                <h5>Rating: {p.Rating}</h5>
+                <button className='product-btn'>Add to cart</button>
+                </div>
+                </>
+            ) 
+            
+        })
+        return result;
+    }
 
-    const showProduct = product.map((p)=>{
-        return(
-            <>
-            <div className='product-container'>
-            <img src={p.Image_URL}/>
-           <h3>{p.Name}</h3>
-            <h5>Price: ${p.Price}</h5>
-            <h5>Rating: {p.Rating}</h5>
-            <button className='product-btn'>Add to cart</button>
-            </div>
-            </>
-        )
-    })
+    //filters product with category 'cake'
+    const cake = product.filter((p)=> p.Category == 'cake');
+    const cakeProduct = productMatching(cake);
+    //console.log(cakeProduct);
+
+     //filters product with category 'savoury'
+    const savoury = product.filter((p)=> p.Category == 'Savoury');
+    const savouryProduct = productMatching(savoury);
+
 
     return(
         <>
         <Nav />
         <h1 style={{ color: 'brown' }}>CAKES</h1>
-        {showProduct}
+        <div className='main-container'>
+        {cakeProduct}
+        </div>
+        <h1 style={{ color: 'brown' }}>SAVOURY</h1>
+        <div className='main-container'>
+        {savouryProduct}
+        </div>
         </>
     )
 }
